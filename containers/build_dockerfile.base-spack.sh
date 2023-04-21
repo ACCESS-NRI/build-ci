19754a0f85b2cb32192e71d754881b21961f0f9c
echus <varvara@echus.co>
2023-04-21 17:05:30 +1000

    Set up S3 buildcache in base-spack container

---
+++ containers/build_dockerfile.base-spack.sh
@@ -0,0 +1,15 @@
+# Ensure S3_ACCESS_KEY_ID and S3_ACCESS_KEY_SECRET
+# are populated in your local environment before building
+# as well as buildcache access private and public keys
+# in ./keys/access-nri.*
+
+DOCKER_BUILDKIT=1 \
+  docker build \
+  --secret id=S3_ACCESS_KEY_ID \
+  --secret id=S3_ACCESS_KEY_SECRET \
+  --secret id=access-nri.priv,src=./keys/access-nri.priv \
+  --secret id=access-nri.pub,src=./keys/access-nri.pub \
+  -f Dockerfile.base-spack \
+  -t base-spack \
+  $@ \
+  . \
