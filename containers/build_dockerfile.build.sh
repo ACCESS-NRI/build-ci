25b43bfb0156ad35bdd8d3f3266835518963440b
echus <varvara@echus.co>
2023-07-11 12:10:34 +1000

    Update build test script with MOM5 example

--- containers/build_dockerfile.build.sh
+++ containers/build_dockerfile.build.sh
@@ -1,10 +1,11 @@
 DOCKER_BUILDKIT=1 \
   docker build \
-  --build-arg PACKAGE="acct" \
+  --build-arg PACKAGE="mom5" \
   --build-arg COMPILER_PACKAGE="intel-oneapi-compilers" \
   --build-arg COMPILER_NAME="intel" \
   --build-arg COMPILER_VERSION="2021.1.2" \
   --build-arg BASE_IMAGE="base-spack:latest" \
   -f Dockerfile.build \
   -t build \
+  $@ \
   . \
