d6057ddd71cdc8ef162943e712d02ed6cd0d1dc8
echus <varvara@echus.co>
2023-07-28 15:52:25 +1000

    Pass buildcache key paths as env var for testing

--- containers/README.md
+++ containers/README.md
@@ -1,12 +1,21 @@
-To build Docker containers, run:
+### Building Docker containers for local testing
 
-S3_ACCESS_KEY_ID=... S3_ACCESS_KEY_SECRET=... ./build_dockerfile.base-spack.sh
-./build_dockerfile.build.sh
+Ensure the following environment variables are populated:
+
+- S3_ACCESS_KEY_ID
+- S3_ACCESS_KEY_SECRET
+- BUILDCACHE_KEY_PRIV_PATH
+- BUILDCACHE_KEY_PUB_PATH
+
+Then run:
+
+    ./build_dockerfile.base-spack.sh
+    ./build_dockerfile.build.sh
 
 To run interactively:
 
-docker run -it build
+    docker run -it build
 
 To test build:
 
-spack -d install --only package --no-checksum mom5%intel
+    spack -d install --only package --no-checksum mom5%intel
--- containers/build_dockerfile.base-spack.sh
+++ containers/build_dockerfile.base-spack.sh
@@ -1,15 +1,16 @@
-# Ensure S3_ACCESS_KEY_ID and S3_ACCESS_KEY_SECRET
-# are populated in your local environment before building
-# as well as buildcache access private and public keys
-# in ./keys/access-nri.*
+# Ensure the following are populated in your local environment before building
+# - S3_ACCESS_KEY_ID
+# - S3_ACCESS_KEY_SECRET
+# - BUILDCACHE_KEY_PRIV_PATH - path to buildcache private key file
+# - BUILDCACHE_KEY_PUB_PATH - path to buildcache public key file
 
 DOCKER_BUILDKIT=1 \
   docker build \
   --build-arg SPACK_PACKAGES_VERSION="main" \
   --secret id=S3_ACCESS_KEY_ID \
   --secret id=S3_ACCESS_KEY_SECRET \
-  --secret id=access-nri.priv,src=./keys/access-nri.priv \
-  --secret id=access-nri.pub,src=./keys/access-nri.pub \
+  --secret id=access-nri.priv,src=${BUILDCACHE_KEY_PRIV_PATH} \
+  --secret id=access-nri.pub,src=${BUILDCACHE_KEY_PUB_PATH} \
   -f Dockerfile.base-spack \
   -t base-spack \
   $@ \
