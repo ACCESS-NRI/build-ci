37570f98a044399de3cc69effb32218ebf935168
Tommy Gatti <tommy.gatti@anu.edu.au>
2023-08-02 09:34:57 +1000

    Removed testing env and lockfiles from repo, optimized Dockerfiles

--- containers/setup-spack-envs.sh
+++ containers/setup-spack-envs.sh
@@ -1,8 +1,7 @@
 for package in "$1"; do
-    spack env create $package
+    spack env create $package $package.spack.yaml
     spack env activate $package
-    spack -d install --add ${COMPILER_PACKAGE}@${COMPILER_VERSION}
-    spack load ${COMPILER_PACKAGE}@${COMPILER_VERSION}
-    spack compiler find --scope site
-    spack -d install --only dependencies ${PACKAGE}%${COMPILER_NAME}@${COMPILER_VERSION}
+    spack -d install --only dependencies --fail-fast
+    spack gc -y
+    spack env deactivate
 done
