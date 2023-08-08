68b615518f14c5fd075c2d94bb51041a7ff40f88
Tommy Gatti <tommy.gatti@anu.edu.au>
2023-08-08 16:20:54 +1000

    Updated dependency install to reference compiler name

--- containers/setup-spack-envs.sh
+++ containers/setup-spack-envs.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# usage: ./setup-spack-envs <compiler name> <compiler version> <models string>
+# usage: ./setup-spack-envs <compiler package> <compiler name> <compiler version> <models string>
 # eg: 
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 4 ]; then
+  echo "Invalid number of arguments: expecting 4"
   exit 1
 fi
 
 COMPILER_PACKAGE="$1"
-COMPILER_VERSION="$2"
-PACKAGES="$3"
+COMPILER_NAME="$2"
+COMPILER_VERSION="$3"
+PACKAGES="$4"
 
 for PACKAGE in $PACKAGES; do
   spack env create $PACKAGE
@@ -16,7 +18,7 @@ for PACKAGE in $PACKAGES; do
   spack -d install -j 4 --add --fail-fast $COMPILER_PACKAGE@$COMPILER_VERSION
   spack load $COMPILER_PACKAGE@$COMPILER_VERSION
   spack compiler find --scope env:$PACKAGE
-  spack -d install -j 4 --add --only dependencies --fail-fast $PACKAGE
+  spack -d install -j 4 --add --only dependencies --fail-fast $PACKAGE%$COMPILER_NAME@$COMPILER_VERSION
   spack gc -y
   spack env deactivate
 done
