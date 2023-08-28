f4e81d15c902e507d1ac867b02378eae8e6337ae
Tommy Gatti <tommy.gatti@anu.edu.au>
2023-08-28 14:33:11 +1000

    All spack installs now explicitly specify an arch

--- containers/setup-spack-envs.sh
+++ containers/setup-spack-envs.sh
@@ -7,9 +7,9 @@ PACKAGES="$1"
 for PACKAGE in $PACKAGES; do
   spack env create $PACKAGE
   spack env activate $PACKAGE
-  spack -d install -j 4 --add --fail-fast $SPACK_ENV_COMPILER_PACKAGE@$SPACK_ENV_COMPILER_VERSION
-  spack load $SPACK_ENV_COMPILER_PACKAGE@$SPACK_ENV_COMPILER_VERSION
+  spack -d install -j 4 --add --fail-fast $SPACK_ENV_COMPILER_PACKAGE@$SPACK_ENV_COMPILER_VERSION arch=$SPACK_ENV_ARCH
+  spack load $SPACK_ENV_COMPILER_PACKAGE@$SPACK_ENV_COMPILER_VERSION arch=$SPACK_ENV_ARCH
   spack compiler find --scope env:$PACKAGE
-  spack -d install -j 4 --add --only dependencies --fail-fast $PACKAGE%$SPACK_ENV_COMPILER_NAME@$SPACK_ENV_COMPILER_VERSION
+  spack -d install -j 4 --add --only dependencies --fail-fast $PACKAGE%$SPACK_ENV_COMPILER_NAME@$SPACK_ENV_COMPILER_VERSION arch=$SPACK_ENV_ARCH
   spack env deactivate
 done
