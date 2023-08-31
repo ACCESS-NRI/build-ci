751a7f88ea60f0e1e2183070d745001bc950b300
Harshula Jayasuriya <harshula.jayasuriya@anu.edu.au>
2023-08-31 16:37:38 +1000

    Install the compiler earlier in the Dockerfile
    * Compiler version set to 2021.2.0 because Gadi /apps does not have 2021.1.2.
    * Explicit bootstrap not required.
    * Clone and add spack_packages last in the base-spack target.

--- util/build_dockerfile.build.sh
+++ util/build_dockerfile.build.sh
@@ -3,7 +3,7 @@ DOCKER_BUILDKIT=1 \
   --build-arg PACKAGE="mom5" \
   --build-arg COMPILER_PACKAGE="intel-oneapi-compilers" \
   --build-arg COMPILER_NAME="intel" \
-  --build-arg COMPILER_VERSION="2021.1.2" \
+  --build-arg COMPILER_VERSION="2021.2.0" \
   --build-arg BASE_IMAGE="base-spack:latest" \
   -f Dockerfile.build \
   -t build \
