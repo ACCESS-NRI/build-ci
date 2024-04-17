5e3034a49c4fb7b3bd01c27c9d9dba6fdc30f271
Harshula Jayasuriya <harshula.jayasuriya@anu.edu.au>
2024-04-17 16:44:16 +1000

    Dockerfile: decouple compiler package version and compiler version (#155)
    * The newer Intel oneAPI compiler packages contain a legacy Fortran compiler
      and a modern LLVM Fortran compiler. The compiler package version does
      not correspond with the legacy Fortran compiler version.

--- containers/setup-spack-envs.sh
+++ containers/setup-spack-envs.sh
@@ -7,9 +7,9 @@ PACKAGES="${1}"
 for PACKAGE in ${PACKAGES}; do
   spack env create ${PACKAGE}
   spack env activate ${PACKAGE}
-  spack -d install -j 4 --add --fail-fast ${SPACK_ENV_COMPILER_PACKAGE}@${SPACK_ENV_COMPILER_VERSION} arch=${SPACK_ENV_ARCH}
-  spack load ${SPACK_ENV_COMPILER_PACKAGE}@${SPACK_ENV_COMPILER_VERSION} arch=${SPACK_ENV_ARCH}
+  spack -d install -j 4 --add --fail-fast ${ENV_COMPILER_PKG_NAME}@${ENV_COMPILER_PKG_VERSION} arch=${ENV_SPACK_ARCH}
+  spack load ${ENV_COMPILER_PKG_NAME}@${ENV_COMPILER_PKG_VERSION} arch=${ENV_SPACK_ARCH}
   spack compiler find --scope env:${PACKAGE}
-  spack -d install -j 4 --add --only dependencies --fail-fast ${PACKAGE}%${SPACK_ENV_COMPILER_NAME}@${SPACK_ENV_COMPILER_VERSION} arch=${SPACK_ENV_ARCH}
+  spack -d install -j 4 --add --only dependencies --fail-fast ${PACKAGE}%${ENV_COMPILER_NAME}@${ENV_COMPILER_VERSION} arch=${ENV_SPACK_ARCH}
   spack env deactivate
 done
