b5e853a2faa7a38e75dd9e1f0f2b3bd16b66d406
Harshula Jayasuriya <harshula.jayasuriya@anu.edu.au>
2024-08-12 14:41:49 +1000

    Dockerfile.base-spack: use target instead of arch (#173)

--- containers/setup-spack-envs.sh
+++ containers/setup-spack-envs.sh
@@ -7,10 +7,10 @@ PACKAGES="${1}"
 for PACKAGE in ${PACKAGES}; do
   spack env create ${PACKAGE}
   spack env activate ${PACKAGE}
-  spack -d install -j 4 --add --fail-fast ${ENV_COMPILER_PKG_NAME}@${ENV_COMPILER_PKG_VERSION} arch=${ENV_SPACK_ARCH}
-  spack load ${ENV_COMPILER_PKG_NAME}@${ENV_COMPILER_PKG_VERSION} arch=${ENV_SPACK_ARCH}
+  spack -d install -j 4 --add --fail-fast ${ENV_COMPILER_PKG_NAME}@${ENV_COMPILER_PKG_VERSION} target=${ENV_SPACK_TARGET}
+  spack load ${ENV_COMPILER_PKG_NAME}@${ENV_COMPILER_PKG_VERSION} target=${ENV_SPACK_TARGET}
   spack compiler find --scope env:${PACKAGE}
-  spack -d install -j 4 --add --only dependencies --fail-fast ${PACKAGE}%${ENV_COMPILER_NAME}@${ENV_COMPILER_VERSION} arch=${ENV_SPACK_ARCH}
+  spack -d install -j 4 --add --only dependencies --fail-fast ${PACKAGE}%${ENV_COMPILER_NAME}@${ENV_COMPILER_VERSION} target=${ENV_SPACK_TARGET}
   # Push any uncached binaries to buildcache
   spack -d buildcache push s3_buildcache "$(spack find --json | jq --raw-output '.[] | (.name + "/" + .hash)')"
   spack env deactivate
